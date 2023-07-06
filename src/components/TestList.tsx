cff854d3b26162ccbe337279220500a2c1471479
NurKhalilov <nurmakhmad.khalilov7@gmail.com>
2023-07-06 17:51:49 +0500

    last

--- src/components/TestList.tsx
+++ src/components/TestList.tsx
@@ -3,50 +3,32 @@ import { Card, Row, Col, Button, Input } from 'antd';
 import { observer } from "mobx-react";
 import todoStore from "../types/TodoStore";
 import { ITodo } from "../types/data";
-import { action } from "mobx";
+import { useEffect } from "react";
 
 
 const TestList: React.FC = observer(() => {
-      const handlePointerDown = (
-        event: React.PointerEvent,
-        item: ITodo,
-        status: string
-      ) => {
-        event.stopPropagation();
-        todoStore.draggedItem = { ...item, status };
-      };
 
-      const handlePointerMove = (event: React.PointerEvent) => {
-        event.preventDefault();
-      };
 
-      const handlePointerUp = (event: React.PointerEvent, status: string) => {
-        event.stopPropagation();
-        if (todoStore.draggedItem) {
-          const updatedItems = todoStore.todos.map((item) => {
-            if (item.id === todoStore.draggedItem!.id) {
-              return { ...item, status };
-            }
-            return item;
-          });
-          todoStore.todos = updatedItems;
-          todoStore.draggedItem = null;
-        }
-      };
+    useEffect(() => {
+      todoStore.fetchData();
+    }, []);
 
-    return <div onPointerMove={handlePointerMove}>
+
+
+
+    return <div onPointerMove={todoStore.handlePointerMove}>
         <Row gutter={[16, 16]} className="container mx-auto p-2">
             {todoStore.cards.map((card, index) => (
           <Col span={4} key={card.status}>
             <Card
               title={card.title}
               className="card bg-sky-500"
-              onPointerUp={(event) => handlePointerUp(event, card.status)}
+              onPointerUp={(event) => todoStore.handlePointerUp(event, card.status)}
             >
               {todoStore.todos
                 .filter((item) => item.status === card.status)
                 .map((todo) => (
-                  <TodoItem key={todo.id} {...todo} handlePointerDown={handlePointerDown} removeTodo={todoStore.removeTodo}  />
+                  <TodoItem key={todo.id} {...todo} handlePointerDown={todoStore.handlePointerDown} removeTodo={todoStore.removeTodo}  />
                 ))}
               
               <Input type="text"
--- src/types/TodoStore.ts
+++ src/types/TodoStore.ts
@@ -1,12 +1,23 @@
-import { makeAutoObservable } from "mobx";
+import { makeAutoObservable, observable, action } from "mobx";
 import { nanoid } from "nanoid";
+import axios from 'axios';
 
-export interface ITodo {
+
+class Todo {
   id: string;
   title: string;
   status: string;
   createdAt: number;
   deletedAt: number | null;
+
+  constructor(id: string, title: string, status: string, createdAt: number, deletedAt: number | null) {
+    this.id = id;
+    this.title = title;
+    this.status = status;
+    this.createdAt = createdAt;
+    this.deletedAt = deletedAt;
+    makeAutoObservable(this);
+  }
 }
 
 export interface ICard {
@@ -16,8 +27,8 @@ export interface ICard {
 }
 
 class TodoStore {
-  todos: ITodo[] = [];
-  draggedItem: ITodo | null = null;
+  todos: Todo[] = [];
+  draggedItem: Todo | null = null;
   cards: ICard[] = [
     { title: 'Backlog', status: 'backlog', value: '' },
   { title: 'To Do', status: 'to-do', value: '' },
@@ -26,9 +37,13 @@ class TodoStore {
   { title: 'Done', status: 'done', value: '' },];
 
   constructor() {
-    makeAutoObservable(this);
+    makeAutoObservable(this, {
+      todos: observable,
+      fetchData: action.bound,
+    });
   }
 
+  
   handleChange = (index: number, value: string) => {
     todoStore.cards = todoStore.cards.map((card, i) => {
       if (i === index) {
@@ -40,13 +55,17 @@ class TodoStore {
 
   addTodo = (status: string, value: string) => {
     if (value) {
-      this.todos.push({
+      let data = {
         id: nanoid(),
         title: value,
         status: status,
         createdAt: Date.now(),
         deletedAt: null,
-      });
+      }
+      this.todos.push(data);
+
+      axios.post("http://127.0.0.1:5000/todos", data)
+      .then(res => console.log(res.data))
 
       this.cards = this.cards.map((card) => {
         if (card.status === status) {
@@ -56,6 +75,58 @@ class TodoStore {
       });
     }
   };
+
+  handlePointerDown = (
+    event: React.PointerEvent,
+    item: Todo,
+    status: string
+  ) => {
+    event.stopPropagation();
+    todoStore.draggedItem = { ...item, status };
+  };
+
+  handlePointerMove = (event: React.PointerEvent) => {
+    event.preventDefault();
+  };
+
+  handlePointerUp = async (event: React.PointerEvent, status: string) => {
+    event.stopPropagation();
+    if (todoStore.draggedItem) {
+      const updatedItems = todoStore.todos.map((item) => {
+        if (item.id === todoStore.draggedItem!.id) {
+          return { ...item, status };
+        }
+        return item;
+      });
+      todoStore.todos = updatedItems;
+      if(todoStore.draggedItem!.id !== null) {
+        try {
+          await axios.put(`http://127.0.0.1:5000/todos/${todoStore.draggedItem!.id}`, { status })
+          .then(res => console.log(res.data));
+          // Assuming your API endpoint for updating a todo item is '/todos/:id'
+          // Replace it with the actual endpoint in your API
+          console.log('Todo item status updated successfully.');
+        } catch (error) {
+          console.error('Error updating todo item status:', error);
+          // Handle error case
+        }
+      }
+      todoStore.draggedItem = null;
+
+      
+    }
+  };
+
+  fetchData = async () => {
+    try {
+      const response = await axios.get("http://127.0.0.1:5000/todos");
+      this.todos = response.data.data;
+      console.log(this.todos);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   
   removeTodo = (id: string): void => {
     this.todos = this.todos.filter(todo => todo.id !== id)
